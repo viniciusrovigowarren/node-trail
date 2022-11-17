a2acdd8fe82cb64d95ef31dfab4635ed1fa07d98
viniciusrovigomedeiros~ <viniciusrovigo9@gmail.com>
2022-11-17 12:33:38 -0300

    feat: user creation

--- src/presentation/customer_route.js
+++ src/presentation/customer_route.js
@@ -3,7 +3,6 @@ import handle from './controllers/customer_controller.js'
 import customerValidators from  './validator/validators.js'
 const CustumerRouter = Router()
 
-
 CustumerRouter.post("/", customerValidators, handle);
 
 export default CustumerRouter;
